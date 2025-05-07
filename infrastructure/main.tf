5cea4228ac9062817aa3791e08249c4f1a1ed03b
Michelozzi2 <michelozzimatthieu@gmail.com>
2025-05-07 11:14:40 +0200

    Trigger CI/CD pipeline after Terraform setup

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -78,6 +78,8 @@ resource "aws_ecs_task_definition" "frontend" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn  # Ajoutez cette ligne
+
   
   container_definitions = jsonencode([
     {
@@ -94,12 +96,36 @@ resource "aws_ecs_task_definition" "frontend" {
   ])
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "backend" {
   family                   = "backend"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn  # Ajoutez cette ligne
+
   
   container_definitions = jsonencode([
     {
